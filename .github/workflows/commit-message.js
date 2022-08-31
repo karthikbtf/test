398c5af194df5618195f1d11389cf0dd305b08f3
Karthik Balachandran <karthik@terrformation.com>
2022-08-31 10:37:45 -0700

    t

--- .github/workflows/commit-message.js
+++ .github/workflows/commit-message.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 
-console.log(process.env);
+console.log('ENV', process.env, 'ENVEND');
 
 const data = `${process.env.JIRA} ${process.env.DESCRIPTION}\n${process.env.DESCRIPTION}`;
+
+console.log('DATA', data, 'DATAEND');
 
 fs.writeFile('/tmp/commit-message', data, (err) => {
   process.stderr.write(err);
